ab5efb255b9b1e6300e1a1dd7e22970ec4afc0ef
Freddie <119540226+SanJammin@users.noreply.github.com>
2025-02-18 09:53:47 +0000

    Basic design
    Think it needs improvement but it's a start

--- index.js
+++ index.js
@@ -7,9 +7,12 @@ function getMenuHtml () {
         menuHtml += `
             <div class="menu-item">
                 <p class="menu-item-emoji">${item.emoji}</p>
-                <h2 class="menu-item-name">${item.name}</h2>
-                <p class="menu-item-ingredients">${item.ingredients.join(", ")}</p>
-                <p class="menu-item-price">$${item.price}</p>
+                <div class="menu-item-description">
+                    <h2 class="menu-item-name">${item.name}</h2>
+                    <p class="menu-item-ingredients">${item.ingredients.join(", ")}</p>
+                    <p class="menu-item-price">$${item.price}</p>
+                </div>
+                <button class="add-item">+</button>
             </div>
         `
     });
