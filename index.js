f7fdb3758c376f18a1d53d0ae2b77b1b9ae56093
Freddie <119540226+SanJammin@users.noreply.github.com>
2025-02-18 09:27:28 +0000

    Add closing quotations

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ function getMenuHtml () {
     menuArray.forEach(item => {
         menuHtml += `
             <div class="menu-item">
-                <p class="menu-item-emoji>${item.emoji}</p>
-                <h2 class="menu-item-name>${item.name}</h2>
-                <p class="menu-item-ingredients>${item.ingredients.join(", ")}</p>
-                <p class="menu-item-price>$${item.price}</p>
+                <p class="menu-item-emoji">${item.emoji}</p>
+                <h2 class="menu-item-name">${item.name}</h2>
+                <p class="menu-item-ingredients">${item.ingredients.join(", ")}</p>
+                <p class="menu-item-price">$${item.price}</p>
             </div>
         `
     });
