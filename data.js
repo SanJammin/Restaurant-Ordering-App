7c28679a468bb8d817cb05ccb93ba432af4d6a0c
Freddie <119540226+SanJammin@users.noreply.github.com>
2025-02-19 12:40:46 +0000

    Add categories for meal deal

--- data.js
+++ data.js
@@ -4,20 +4,23 @@ export const menuArray = [
         ingredients: ["pepperoni", "mushroom", "mozarella"],
         price: 14,
         emoji: "🍕",
-        id: 0
+        id: 0,
+        category: "food"
     },
     {
         name: "Hamburger",
         ingredients: ["beef", "cheese", "lettuce"],
         price: 12,
         emoji: "🍔",
-        id: 1
+        id: 1,
+        category: "food"
     },
         {
         name: "Beer",
         ingredients: ["grain", "hops", "yeast", "water"],
         price: 12,
         emoji: "🍺",
-        id: 2
+        id: 2,
+        category: "drink"
     }
 ];
